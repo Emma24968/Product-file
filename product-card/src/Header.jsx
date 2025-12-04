710e5d3ae14de6e8ce712c8f8434b34e5ffde16a
Emma24968 <emnabs12@gmail.com>
2025-12-04 18:42:56 +0100

    I started styling the header section

--- product-card/src/Header.jsx
+++ product-card/src/Header.jsx
@@ -1,13 +1,20 @@
-export const Header = ()=>{
-    return(
-        <div>
-            
-            <div><h1>Ourshop</h1></div>
-            <div>
-                <li><a href="#">Products</a></li>
-                <li><a href="#">Cart</a></li>
-                <li><a href="#">Login</a></li>
-            </div>
-        </div>
-    )
-}
+export const Header = () => {
+  return (
+    <div className="Header-Section">
+      <div>
+        <h1>Ourshop</h1>
+      </div>
+      <div>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Cart</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </div>
+    </div>
+  );
+};
